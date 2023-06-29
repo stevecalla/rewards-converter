4f075ba30b4891ee59019008479d13b63a7469f8
Steve Calla <callasteven@gmail.com>
2023-06-29 06:55:03 -0600

    Update test to ensure miles or cash convert accurately

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,14 +1,14 @@
 public class RewardValue {
-    final private double milesToCashConversionRate = 0.0035;
+    final private double conversionRate = 0.0035;
     private double cashValue;
     private int milesValue;
     public RewardValue(double cashValue) {
         this.cashValue = cashValue;
-        milesValue = (int) (cashValue / milesToCashConversionRate);
+        milesValue = (int) (cashValue / conversionRate);
     }
     public RewardValue(int milesValue) {
         this.milesValue = milesValue;
-        cashValue = milesValue * milesToCashConversionRate;
+        cashValue = milesValue * conversionRate;
     }
     public double getCashValue() {
         return cashValue;
